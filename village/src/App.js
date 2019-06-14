b722f8488dd739e715dc378bf8ebf9b0134f1bd2
basilcea <ogbonna.basil3@gmmail.com>
2019-06-14 09:13:04 +0100

    - Create get Method for smurfs - Call get method in componentDidMount - Display list of smurfs on the screen - Create a post method with forms

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -9,15 +9,30 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      error:''
     };
+  }
+  getSmurfs = async() =>{
+    try {
+      const axoisData = await axios.get("http://localhost:3333/smurfs");
+      this.setState({ smurfs: axoisData.data });
+    } catch (err) {
+      this.setState({
+        error: err.message
+      });
+    }
+  }
+  componentDidMount(){
+    this.getSmurfs();
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
+    console.log(this.state.smurfs)
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm getSmurfs={this.getSmurfs} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -6,13 +7,25 @@ class SmurfForm extends Component {
     this.state = {
       name: '',
       age: '',
-      height: ''
+      height: '',
+      error:'',
     };
   }
 
-  addSmurf = event => {
+  addSmurf = async(event) => {
     event.preventDefault();
     // add code to create the smurf using the api
+    try{
+      await axios.post('http://localhost:3333/smurfs',
+      {name:this.state.name, age:this.state.age ,height:this.state.height})
+      return this.props.getSmurfs()
+    }
+    catch(err){
+      this.setState({
+        error:err.message
+      })
+
+    }
 
     this.setState({
       name: '',
